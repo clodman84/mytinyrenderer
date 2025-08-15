d87be533395103e3ed3db206974f5cca10a82fb9
clodman84 <cshourin@gmail.com>
2025-08-15 14:39:56 +0530

    add build

---
+++ build/build_files/obj/x64/Debug/mytinyrenderer/main.d
@@ -0,0 +1,8 @@
+obj/x64/Debug/mytinyrenderer/main.o: ../../src/main.cpp \
+ /usr/include/stdc-predef.h ../external/raylib-master/src/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
+ ../../include/resource_dir.h
+/usr/include/stdc-predef.h:
+../external/raylib-master/src/raylib.h:
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h:
+../../include/resource_dir.h:
